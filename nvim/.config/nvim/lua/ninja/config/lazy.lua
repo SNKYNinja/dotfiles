78868365edcba811dbb96eee2736beaf5582c3cd
aditya <aditya.chaudhary0523@gmail.com>
2024-12-27 22:45:48 +0530

    `neovim`

---
+++ nvim/.config/nvim/lua/ninja/config/lazy.lua
@@ -0,0 +1,65 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = {
+        { import = "ninja.themes" },
+        { -- Highlight todo, notes, etc in comments
+            "folke/todo-comments.nvim",
+            event = "VimEnter",
+            dependencies = { "nvim-lua/plenary.nvim" },
+            opts = { signs = false },
+        },
+        { "tpope/vim-sleuth" },      -- Vim Sleuth (Detect tabstop and shiftwidth automatically)
+
+        { "numToStr/Comment.nvim" }, -- Commenting plugin
+
+        { import = "ninja.plugins" },
+    },
+    change_detection = {
+        -- automatically check for config file changes and reload the ui
+        enabled = false,
+        notify = false, -- get a notification when changes are found
+    },
+}, {
+    rocks = {
+        enabled = false,
+    },
+    ui = {
+        -- If you are using a Nerd Font: set icons to an empty table which will use the
+        -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+        icons = vim.g.have_nerd_font and {} or {
+            cmd = "⌘",
+            config = "🛠",
+            event = "📅",
+            ft = "📂",
+            init = "⚙",
+            keys = "🗝",
+            plugin = "🔌",
+            runtime = "💻",
+            require = "🌙",
+            source = "📄",
+            start = "🚀",
+            task = "📌",
+            lazy = "💤 ",
+        },
+    },
+})
