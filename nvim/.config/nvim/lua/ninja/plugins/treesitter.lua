240d4d8c9938004ba6804d2dc6e6bdf4c88dc796
aditya <aditya.chaudhary0523@gmail.com>
2025-01-13 23:31:38 +0530

    `neovim`

--- nvim/.config/nvim/lua/ninja/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/ninja/plugins/treesitter.lua
@@ -25,6 +25,10 @@ return {
                     "java",
                     "dockerfile",
                 },
+                ignore_install = {},
+                sync_install = false,
+                auto_install = false,
+                modules = {},
                 highlight = {
                     enable = true,
                     use_languagetree = true,
@@ -34,4 +38,3 @@ return {
         end,
     },
 }
-
